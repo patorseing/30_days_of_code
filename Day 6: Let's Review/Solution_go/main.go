fd557e0bc650792dbb43231c215255aa89911abf
Napatchol Thaipanich <patorseing@gmail.com>
2020-12-07 20:15:12 +0700

    Day 6: finish go

--- Day 6: Let's Review/Solution_go/main.go
+++ Day 6: Let's Review/Solution_go/main.go
@@ -1,28 +1,41 @@
+// https://github.com/xeoneux/30-Days-of-Code/blob/master/6%20-%20Let's%20Review/Solution.go
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
-	//Enter your code here. Read input from STDIN. Print output to STDOUT
-	var count int
-	fmt.Scan(&count)
-	for i := 0; i < count; i++ {
-		var str string
-		fmt.Scan(&str)
-		// var first string
-		var first bytes.Buffer
-		// var second string
-		var second bytes.Buffer
-		for j, ch := range str {
-			if j%2 == 0 {
-				first.WriteRune(ch)
-			} else {
-				second.WriteRune(ch)
-			}
-		}
-		fmt.Println(first.String() + " " + second.String())
+const (
+	EVEN = iota
+	ODD
+)
+
+func print(oddOrEven int, words string) {
+	for i := oddOrEven; i < len(words); i += 2 {
+		fmt.Printf("%c", words[i])
 	}
+	fmt.Print(" ")
+}
+
+func main() {
+	var T int
+
+	fmt.Scan(&T)
+	words := make([]string, T)
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < T; i++ {
+		words[i], _ = reader.ReadString('\n')
+		words[i] = strings.TrimSuffix(words[i], "\n")
+	}
+
+	for k := range words {
+		print(EVEN, words[k])
+		print(ODD, words[k])
+		fmt.Printf("\n")
+	}
+
 }
